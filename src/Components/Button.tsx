f6f283846308d9155668ac9f47a9abe4e0d049a1
Gabriel Souza <gabrielss_@outlook.com>
2021-10-11 02:07:54 -0300

    dia 1

---
+++ src/Components/Button.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Button = () => {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return <button onClick={increment}>{counter}</button>;
+};
+
+export default Button;
